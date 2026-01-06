e7390341bad4d49ca7c351a94c4e82ac25a66183
KanishkSingh13 <103627053+KanishkSingh13@users.noreply.github.com>
2026-01-06 15:02:36 +0000

    updated conversion and added a file

--- 01_Basics/03_datatype_conversions.js
+++ 01_Basics/03_datatype_conversions.js
@@ -25,9 +25,44 @@ console.log(typeof booleanIsLoggedIn)*/
 
 
 
-let someNumber =33
+/*let someNumber =33
 
 let stringnumber = String(someNumber)
 
 console.log(typeof stringnumber)
-console.log(stringnumber)
+console.log(stringnumber)*/
+
+
+
+
+
+
+
+// *******************Operations*****************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " Kanishk"
+
+let str3 = str1 + str2
+console.log(str3)
+
+/*console.log("1" + 2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2") */
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2+2
+
+console.log(num1, num2, num3)
+
+let gameCounter = 100
+
+gameCounter ++
+
+console.log(gameCounter)
