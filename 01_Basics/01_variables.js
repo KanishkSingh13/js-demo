f2b65df264dfa96500277426cedd1bdc746130cf
KanishkSingh13 <103627053+KanishkSingh13@users.noreply.github.com>
2026-01-06 13:26:40 +0000

    Lecture 2 completed

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountID = 123456
+let accountEmail = "kanishksingh07.13@gmail.com"
+var accountPassword = "myStrongPassword123"
+accountCity = "New Delhi"
+let accountState
+
+console.log("Account ID:", accountID)
+
+console.table([accountEmail, accountPassword, accountCity ,accountID , accountState])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict" // treats all code as newer version of JavaScript
+
+// alert(3+3) // we are using it on nodejs, so alert won't work, but it works in browsers
+
+console.log(3+3)
+
+console.log("Kanishk")
+
+let name = "Kanishk" 
+let age = 18
+let IsUserLoggedIn = true
+
+console.log(typeof null)
