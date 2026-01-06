e4762726a9e8fc38656d5f6f84df7861ac491262
KanishkSingh13 <103627053+KanishkSingh13@users.noreply.github.com>
2026-01-06 13:48:08 +0000

    Conversions of datatypes

---
+++ 01_Basics/03_datatype_conversions.js
@@ -0,0 +1,33 @@
+/*let score = true
+
+console.log(typeof score) 
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)*/
+
+
+
+
+
+
+
+/*let isLoggedIn = "yes bhai yes"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)*/
+
+
+
+
+
+let someNumber =33
+
+let stringnumber = String(someNumber)
+
+console.log(typeof stringnumber)
+console.log(stringnumber)
