548cf332fedd63e33fda75c07187a20ed5feca9b
KanishkSingh13 <103627053+KanishkSingh13@users.noreply.github.com>
2026-01-07 15:12:05 +0000

    added comparison and started datatypes summary

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -1,6 +1,21 @@
-console.log(2>1)
+/* console.log(2>1)
 console.log(2<1)
 console.log(2>=1)
 console.log(2<=1)
 console.log(2==1)
-console.log(2!=1)
+console.log(2!=1) */
+
+/*console.log("2">1)
+console.log("02">1)*/
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0) 
+// null is converted to 0 only in >= or <= comparisons but in == it is not converted.
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+// undefined is converted to NaN in all comparisons except != and == where it is not converted.
+
+console.log("2"==2)
